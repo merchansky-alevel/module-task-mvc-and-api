ad0dbbae4b98e863a48c5e3ffaf37bd61343072f
Anton Petrenko <petrenko.anton22@gmail.com>
2018-09-30 23:33:43 +0300

    added IServerService

---
+++ AUdependencies/Services/IServerService.cs
@@ -0,0 +1,17 @@
+﻿using AUdependencies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUdependencies.Services
+{
+    public interface IServerService
+    {
+        Server Get(int id);
+        void Add();
+        void Update(Server server);
+        void Delete(int id);
+    }
+}
